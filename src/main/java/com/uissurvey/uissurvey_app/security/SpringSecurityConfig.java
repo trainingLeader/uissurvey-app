573ac748ebde8aaf3884c2ca8023fd703d35df99
trainingLeader <131613955+trainingLeader@users.noreply.github.com>
2024-10-07 22:18:05 -0500

    feat: :sparkles: Add new features
    Add JWT auth

--- src/main/java/com/uissurvey/uissurvey_app/security/SpringSecurityConfig.java
+++ src/main/java/com/uissurvey/uissurvey_app/security/SpringSecurityConfig.java
@@ -1,17 +1,28 @@
 package com.uissurvey.uissurvey_app.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import com.uissurvey.uissurvey_app.infrastructure.utils.filter.JwtAuthenticationFilter;
+
 @Configuration
 public class SpringSecurityConfig {
+    @Autowired
+    private AuthenticationConfiguration authenticationConfiguration;
 
+    @Bean
+    AuthenticationManager authenticationManager() throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
@@ -24,6 +35,7 @@ public class SpringSecurityConfig {
                 .requestMatchers(HttpMethod.POST,"/api/users/register").permitAll()
                 .requestMatchers("/api/surveyanswer").permitAll()
                 .anyRequest().authenticated())
+                .addFilter(new JwtAuthenticationFilter(authenticationManager()))
                 .csrf(config -> config.disable())
                 .sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .build();
